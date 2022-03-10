527af38781be1265a57f7f46590f1e9b9a5922d8
Kalo-py <98994129+Kalo-py@users.noreply.github.com>
2022-03-10 20:18:07 -0300

    Update TSIEj01Main.java

--- Prueba/ejemplo01/TSIEj01Main.java
+++ Prueba/ejemplo01/TSIEj01Main.java
@@ -6,6 +6,9 @@ public class TSIEj01Main {
 		Persona p1 = new Persona(12345, "Carlos", "Reyes", "carlosservian09@gmail.com");
 		System.out.println("Nombre y Apellido: "+p1.getNombre()+p1.getApellido());
 		
+		Alumno al1 = new Alumno (12345, "Carlos", "Reyes", "carlosservian09@gmail.com", "sumatricula");
+		System.out.println("Alumno, Nombre y apellido: "+al1.getNombre()+" "+al1.getApellido()+" ");
+		
 		Integer edad = 20;
 		Boolean esm = edad>18;
 		if (esm) {
