7f26ef65402fa371ed0fdbc9db31a7a89fc6406c
unknown <carlosservian09@gmail.com>
2022-03-10 19:50:18 -0300

    agregando una condicional

--- Prueba/ejemplo01/TSIEj01Main.java
+++ Prueba/ejemplo01/TSIEj01Main.java
@@ -5,6 +5,15 @@ public class TSIEj01Main {
 	public static void main(String[] args) {
 		Persona p1 = new Persona(12345, "Carlos", "Reyes", "carlosservian09@gmail.com");
 		System.out.println("Nombre y Apellido: "+p1.getNombre()+p1.getApellido());
+		
+		Integer edad = 20;
+		Boolean esm = edad>18;
+		if (esm) {
+			System.out.println("Es mayor de edad");
+		}
+		else {
+			System.out.println("No es mayor de edad");
+		}
 	}
 
 }
